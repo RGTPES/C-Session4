344b8cd40d5dad61509d7a56900c07e6d456bd05
RGTPES <Maiduyanh7@gmail.com>
2024-11-24 22:40:09 +0700

    session4_6.cpp

--- session4_6.cpp
+++ session4_6.cpp
@@ -1,22 +1,23 @@
 #include<stdio.h>
 int main(){
-	int Bmonth,Emonth,consume;
+	int Bmonth,Emonth,consume,sum,money;
 	printf("nhap so dien su dung luc dau thang\n");
 	scanf("%d",&Bmonth);
 	printf("nhap so dien su dung luc cuoi thang\n");
 	scanf("%d",&Emonth);
 	consume = Emonth - Bmonth;
 	if (consume >= 0 && consume < 50){
-		printf("so tien dien thag nay la 10000");
+		money = 10000;
 		
 	}else if(consume <100){
-		printf("so tien dien thang nay la 15000");
+		money = 15000;
 	}else if(consume<150){
-		printf("so tien dien thang nay la 20000");
+		money = 20000;
 	}else if(consume<200){
-		printf("so tien dien thang nay la 25000");
+		money = 25000;
 	}else{
-		printf("so tien dien thang nay la 30000");
-		
+		money = 30000;	
 	}
+	sum =consume * money;
+	printf("%d",sum);
 }
